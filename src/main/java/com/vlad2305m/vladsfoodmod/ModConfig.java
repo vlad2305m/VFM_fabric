949097d08a4af2c9c0c661e62d2d00f53901975c
vlad2305m <vlad2305m@gmail.com>
2021-01-21 12:05:22 +0100

    Removed Bs except 6&12 because there is no data on them in USDA; started filling database ==> v0.1.1

--- src/main/java/com/vlad2305m/vladsfoodmod/ModConfig.java
+++ src/main/java/com/vlad2305m/vladsfoodmod/ModConfig.java
@@ -57,11 +57,11 @@ public class ModConfig extends PartitioningSerializer.GlobalData {
         @ConfigEntry.BoundedDiscrete(min = -1000, max = 2000)
         private Long longSlider = 500L;*/
 
-        @Comment("one and only food database for this mod (in grams)")
+        @Comment("one and only food database for this mod (in grams) [delete this file to reset]")
         public Map<String, NutrientStore> nutrientStoreMap = new HashMap<String, NutrientStore>() {{
-            put(Items.APPLE.getTranslationKey(), new NutrientStore());
-            put(Items.BAKED_POTATO.getTranslationKey(), new NutrientStore());
-            put(Items.BEEF.getTranslationKey(), new NutrientStore());
+            put(Items.APPLE.getTranslationKey(), new NutrientStore(1f,13.81f,0.26f,0.17f,85.56f,3e-6f,0f,0.18e-3f,2.2e-6f,0f,0f,4.6e-3f,107e-3f,6e-3f,11e-3f,5e-3f,0.027e-3f,0.12e-3f,0f));
+            put(Items.BAKED_POTATO.getTranslationKey(), new NutrientStore(1f,21.05f,2.49f,0.13f,74.55f,1e-6f,0f,0.04e-3f,2e-6f,0.31e-3f,0f,9.6e-3f,533e-3f,15e-3f,70e-3f,28e-3f,0.118e-3f,1.08e-3f,0.4f));
+            put(Items.BEEF.getTranslationKey(), new NutrientStore(1f,0f,20.85f,7f,71.72f,4e-6f,0.1e-6f,0.17e-3f,0.5e-6f,0.383e-3f,2.23e-6f,0e-3f,336e-3f,10e-3f,192e-3f,21e-3f,0.075e-3f,2.33e-3f,17.1f));
             put(Items.BEETROOT.getTranslationKey(), new NutrientStore());
             put(Items.BEETROOT_SOUP.getTranslationKey(), new NutrientStore());
             put(Items.BREAD.getTranslationKey(), new NutrientStore());
